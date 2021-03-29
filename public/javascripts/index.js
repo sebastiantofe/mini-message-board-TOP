d6aa63e4b4551b4c3f3e870918122ebcf8f254d5
Sebastian Torrado <sebastiantofe@gmail.com>
2021-03-29 17:28:10 -0500

    Completed project

---
+++ public/javascripts/index.js
@@ -0,0 +1,3 @@
+document.getElementById('new-message').onclick = function() {
+    location.href = 'http://localhost:3000/new'
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Armando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages });
 });
 
+router.get('/new', function (req, res) {
+  res.sendFile('/public/form.html', { 
+    root: path.join(__dirname, '/..')
+   });
+});
+
+router.post('/new', function (req, res) {
+
+  const message = req.body.message;
+  const name = req.body.name;
+
+  messages.push({
+    user: name,
+    text: message,
+    added: new Date()
+  });
+  
+  res.redirect('/');
+})
+
+
+
+
 module.exports = router;
+
+
